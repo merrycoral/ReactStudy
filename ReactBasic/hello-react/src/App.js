396c0b5b14eda3f0dd46b54f637a32a8261c0109
admin <admin@DESKTOP-FAENBO2>
2021-03-23 13:08:25 +0900

    210323 리액트 공부하자!

---
+++ ReactBasic/hello-react/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const name = '밀크티';
+  return <div className="react">{name}</div>;
+}
+
+export default App;
+
+/* import React, { Component } from 'react';
+import './App.css'
+import MyName from './MyName';
+
+class App extends Component {
+  render() {
+   return (
+     <div className="App">
+      리액트
+    </div> 
+    <MyName name="리액트" />
+   );
+  }
+}
+*/
+
+
---
+++ ReactBasic/hello-react/src/condition.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Condition extends Component {
+	render() {
+		return (
+			<div>
+				
+			</div>
+		);
+	}
+}
+
+export default Condition;
