52e44d1c17e146c8f405f7b362f865c7dfaf1752
merrycoral <merrycoral@gmail.com>
2021-03-24 14:03:16 +0900

    임의의 메서드 생성하기

--- ReactBasic/hello-react/src/EventPractice.js
+++ ReactBasic/hello-react/src/EventPractice.js
@@ -5,10 +5,31 @@ class EventPractice extends Component {
 	state ={
 		message: ''
 	}
+
+	constructor(props) {
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleChange(e) {
+		this.setState({
+			message: e.target.value
+		});
+	}
+
+	handleClick() {
+		alert(this.state.message);
+		this.setState({
+			message: ''
+		});
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>이벤트 연습</h1>
+
 				<input
 					type="text"
 					name="message"
