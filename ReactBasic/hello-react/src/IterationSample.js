10c3a9ce388a0c47d5c9c129fba65e5259828520
merrycoral <merrycoral@gmail.com>
2021-03-25 15:37:19 +0900

    컴포넌트 반복 - 키 설정

--- ReactBasic/hello-react/src/IterationSample.js
+++ ReactBasic/hello-react/src/IterationSample.js
@@ -1,4 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const IterationSample = () => {
+	const [names, setNames] = useState([
+		{ id: 1, text: '밀크티'},
+		{ id: 2, text: '버블티'},
+		{ id: 3, text: '타로밀크티'},
+		{ id: 4, text: '딸기밀크티'}
+	]);
+	const [inputText, setInputText] = useState('');
+	const [nextId, setNextId] = useState(5); 
+
+	const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
+	return <ul>{namesList}</ul>;
+}
+export default IterationSample;
+
+
+/* import React from 'react';
 
 const IterationSample = () => {
 	const names = ['홍차', '녹차', '보이차', '우롱차'];
@@ -7,4 +25,4 @@ const IterationSample = () => {
 
 };
 
-export default IterationSample;
+export default IterationSample; */
