7611ff5f451bb99dbe8e8ecddb74d161bebc552b
merrycoral <merrycoral@gmail.com>
2021-03-30 15:55:53 +0900

    Hooks - useMemo before

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import Counter from './Counter';
 import Info from './Info';
+import Average from './Average';
 
 const App = () => {
   const [visible, setVisible] = useState(false);
   return (
     <div>
+      <Average/>
+      <hr/>
       <Counter/>
       <hr/>
       <button onClick={() => {
