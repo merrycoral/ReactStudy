6572c1868a869ca5e906721f4a35390c90a6b2ce
merrycoral <merrycoral@gmail.com>
2021-03-30 14:44:44 +0900

    Hooks - useReducer로 카운터

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-//import Counter from './Counter';
+import Counter from './Counter';
 import Info from './Info';
 
 const App = () => {
   const [visible, setVisible] = useState(false);
   return (
     <div>
+      <Counter/>
+      <hr/>
       <button onClick={() => {
           setVisible(!visible);
       }}
