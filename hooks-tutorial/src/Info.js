8b0720a412d352b44982aa26988141400d535147
merrycoral <merrycoral@gmail.com>
2021-04-01 13:29:07 +0900

    css module 적용 완료

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,30 +1,27 @@
-import React, { useState, useEffect, Component } from 'react';
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+	return {
+		...state,
+		[action.name]: action.value
+	};
+}
 
 const Info = () => {
-	const [name, setName] = useState('');
-	const [nickname, setNickname] = useState('');
-	useEffect(() => {
-		console.log('effect');
-		console.log(name);
-		return () => {
-			console.log('cleanup');
-			console.log(name);
-		}
+	const [state, dispatch] = useReducer(reducer, { 
+		name: '',
+		nickname: '' 
 	});
-
-	const onChangeName = e => {
-		setName(e.target.value);
-	}
-
-	const onChangeNickname = e => {
-		setNickname(e.target.value);
-	}
+	const { name, nickname } = state;
+	const onChange = e => {
+		dispatch(e.target);
+	};
 
 	return (
 		<div>
 			<div>
-				<input value={name} onChange={onChangeName} />
-				<input value={nickname} onChange={onChangeNickname} />
+				<input value={name} onChange={onChange} />
+				<input value={nickname} onChange={onChange} />
 			</div>
 			<div>
 				<div>
@@ -35,8 +32,8 @@ const Info = () => {
 				</div>
 			</div>
 		</div>
-	)
-}
+	);
+};
 
 
 export default Info;
--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -5,6 +5,7 @@ const CSSModule = () => {
 	return (
 		<div className={styles.wrapper}>
 			안녕하세요, 저는 <span className="something">CSS Module!</span>
+			{console.log(styles)}
 		</div>
 	);
 };
