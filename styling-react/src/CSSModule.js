ae1ee0e6b692829bc08533bd44f57e7ed69bbc5e
merrycoral <merrycoral@gmail.com>
2021-04-01 13:52:53 +0900

    CSS Module - className 2개 이상 사용

--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -3,8 +3,8 @@ import styles from './CSSModule.module.css';
 
 const CSSModule = () => {
 	return (
-		<div className={styles.wrapper}>
-			안녕하세요, 저는 <span className="something">CSS Module!</span>
+		<div className={`${styles.wrapper} ${styles.inverted}`}>
+			안녕하세요, 저는 <span className="something"> CSS Module!</span>
 			{console.log(styles)}
 		</div>
 	);
