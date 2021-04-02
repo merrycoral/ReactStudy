508091c10553b76a430bdec6fe62f3049d7ae8e4
merrycoral <merrycoral@gmail.com>
2021-04-02 15:59:58 +0900

    Todo app 클릭하면 체크 취소 구현

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -2,14 +2,19 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = ({ todos, onRemove }) => {
-    return (
-        <div className="TodoList">
-            {todos.map(todo => (
-                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
-            ))}
-        </div>
-    );
+const TodoList = ({ todos, onRemove, onToggle }) => {
+  return (
+    <div className="TodoList">
+      {todos.map(todo => (
+        <TodoListItem 
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
