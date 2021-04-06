c463864b8acb6f118499c85de16a727850f5d9fb
merrycoral <merrycoral@gmail.com>
2021-04-06 14:42:48 +0900

    Link component로 링크 걸기

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      {/* 여기 li 반복되는 부분도 쉽게 반복문이 가능할까?
+      아니면 패스가 다 달라서 이게 최선일까? */}
+      <hr/>
+      <Route path="/" component={Home} ﻿exact={true} />
+      <Route path="/about" component={About} />
     </div>
   );
 }
