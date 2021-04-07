0380d40a410e0a0210d4dea74c4b54d9e00364d5
merrycoral <merrycoral@gmail.com>
2021-04-07 14:40:58 +0900

    React Router - Switch

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 import Profiles from './Profiles';
@@ -23,10 +23,21 @@ const App = () => {
         </li>
       </ul>
       <hr/>
-      <Route path="/" component={Home} ﻿exact={true} />
-      ﻿<Route path={['/about', '/info']} component={About} />
-      <Route path="/profiles" component={Profiles} />
-      <Route path="/HistorySample" component={HistorySample} />
+      <Switch>
+        <Route path="/" component={Home} ﻿exact={true} />
+        ﻿<Route path={['/about', '/info']} component={About} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/HistorySample" component={HistorySample} />
+        <Route
+          //path를 정의하지 않은 모든 상황에 렌더링 될 화면
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다:</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
     </div>
   );
 }
